f99cc1d58e30a753fdaf805bc573ac208273ceb2
Beavan <xinqiangc@gmail.com>
2019-04-26 13:54:28 +0800

    Create k8s_join.sh
    k8s_join.sh

---
+++ k8s_join.sh
@@ -0,0 +1,11 @@
+# gcrxio mirror 
+#kubeadm config images list |grep proxy |sed -e 's/^/docker pull /g' -e 's#k8s.gcr.io#gcrxio#g' |sh -x
+#kubeadm config images list |grep pause |sed -e 's/^/docker pull /g' -e 's#k8s.gcr.io#gcrxio#g' |sh -x
+#docker images |grep gcrxio |awk '{print "docker tag ",$1":"$2,$1":"$2}' |sed -e 's#gcrxio#k8s.gcr.io#2' |sh -x
+#docker images |grep gcrxio |awk '{print "docker rmi ", $1":"$2}' |sh -x
+
+# aliyuncs mirror 
+kubeadm config images list |grep proxy |sed -e 's/^/docker pull /g' -e 's#k8s.gcr.io#registry.aliyuncs.com/google_containers#g' |sh -x
+kubeadm config images list |grep pause |sed -e 's/^/docker pull /g' -e 's#k8s.gcr.io#registry.aliyuncs.com/google_containers#g' |sh -x
+docker images |grep aliyuncs |awk '{print "docker tag ",$1":"$2,$1":"$2}' |sed -e 's#registry.aliyuncs.com/google_containers#k8s.gcr.io#2' |sh -x
+docker images |grep aliyuncs |awk '{print "docker rmi ", $1":"$2}' |sh -x
