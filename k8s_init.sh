497a6be03f77611d236d040796e485de70e1dd24
maxinqiang <maxinqiang@skyguard.com.cn>
2023-02-25 17:36:02 +0800

    update k8s_init.sh

--- k8s_init.sh
+++ k8s_init.sh
@@ -1,9 +1,21 @@
-# gcrxio mirror
-#kubeadm config images list |sed -e 's/^/docker pull /g' -e 's#k8s.gcr.io#gcrxio#g' |sh -x
-#docker images |grep gcrxio |awk '{print "docker tag ",$1":"$2,$1":"$2}' |sed -e 's#gcrxio#k8s.gcr.io#2' |sh -x
-#docker images |grep gcrxio |awk '{print "docker rmi ", $1":"$2}' |sh -x
+#!/bin/bash
+  
+match_repo=registry.k8s.io
+replace_repo=registry.aliyuncs.com/google_containers
 
-# aliyun mirror
-kubeadm config images list |sed -e 's/^/docker pull /g' -e 's#k8s.gcr.io#registry.aliyuncs.com/google_containers#g' |sh -x
-docker images |grep aliyuncs |awk '{print "docker tag ",$1":"$2,$1":"$2}' |sed -e 's#registry.aliyuncs.com/google_containers#k8s.gcr.io#2' |sh -x
-docker images |grep aliyuncs |awk '{print "docker rmi ", $1":"$2}' |sh -x
+if [[ -n $1 ]];then
+        match_repo=$1
+fi
+
+echo "match repo: $match_repo"
+echo "replaced repo: $replace_repo"
+
+## aliyun mirror
+kubeadm config images list |grep -v coredns |sed -e 's/^/docker pull /g' -e "s#$match_repo#$replace_repo#g" |sh -x
+# coredns单独处理/路径差异
+kubeadm config images list |grep coredns |sed -e 's/^/docker pull /g' -e "s#$match_repo/coredns#$replace_repo#g" |sh -x
+# update tags
+docker images |grep aliyuncs |awk '{print "docker tag",$1":"$2,$1":"$2}' |sed -e "s#$replace_repo#$match_repo#2" |sh -x
+# del temp images 
+docker images |grep aliyuncs |awk '{print "docker rmi", $1":"$2}' |sh -x
+
